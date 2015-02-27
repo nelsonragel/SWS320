089180b1fc96e89e20471cf0bad1f153c0cb1b8c
Nelson Ragel <nelsonragel@users.noreply.github.com>
2015-02-27 11:42:45 -0500

    Update Student _Database.sql

--- Student _Database.sql
+++ Student _Database.sql
@@ -6,5 +6,5 @@ CREATE TABLE Student(Student_Id int , FirstName VARCHAR(255), LastName VARCHAR(2
 
 INSERT INTO Students VALUES(300645794, "Nelson", "Ragel", "Financial", 1);
 INSERT INTO Students VALUES(300689548, "Ari", "Bala", "None", 3);
-INSERT INTO Students VALUES(300456187, "Fatima", "Alhasim", "Academic", 4);
+INSERT INTO Students VALUES(300456187, "Fatima", "Alhashim", "Academic", 4);
 INSERT INTO Students VALUES(300321564, "Denize", "Daza", "None", 5);
