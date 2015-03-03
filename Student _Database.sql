42cb0f77972f11858cfb60fabcb50791e722a0a4
Nelson Ragel <nelsonragel@users.noreply.github.com>
2015-03-03 07:15:00 -0500

    Update Student _Database.sql

--- Student _Database.sql
+++ Student _Database.sql
@@ -2,7 +2,7 @@ CREATE DATABASE Student;
 
 USE Student;
 
-CREATE TABLE Student(Student_Id int , FirstName VARCHAR(255), LastName VARCHAR(255), Holds VARCHAR(255), Semester int);
+CREATE TABLE Students(Student_Id int , FirstName VARCHAR(255), LastName VARCHAR(255), Holds VARCHAR(255), Semester int);
 
 INSERT INTO Students VALUES(300645794, "Nelson", "Ragel", "Financial", 1);
 INSERT INTO Students VALUES(300689548, "Ari", "Bala", "None", 3);
